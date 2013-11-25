354a2821c6f9fb4f25a7dd8bf20d2cc1af351b1c
kramfs <kramfs@rs-api-env.cs1cloud.internal>
2013-11-25 17:24:30 +0800

    Added a hello world message to the default.rb recipe

---
+++ cookbooks/timeout_test/metadata.rb
@@ -0,0 +1,9 @@
+name             'timeout_test'
+maintainer       'YOUR_COMPANY_NAME'
+maintainer_email 'YOUR_EMAIL'
+license          'All rights reserved'
+description      'Installs/Configures timeout_test'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
+
+recipe "timeout_test::default","Prints hello world output"  
