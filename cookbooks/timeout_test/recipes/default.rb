a1de3c76af16ead93b1fdb8b6e8c4e3084e97580
kramfs <kramfs@rs-api-env.cs1cloud.internal>
2013-11-27 12:09:55 +0800

    Added marker bein/end

--- cookbooks/timeout_test/recipes/default.rb
+++ cookbooks/timeout_test/recipes/default.rb
@@ -7,5 +7,6 @@
 # All rights reserved - Do Not Redistribute
 #
 
-rightscale_marker
+rightscale_marker :begin
 log "Hello World! You should try Chef with RightScale!"
+rightscale_marker :end
--- cookbooks/timeout_test/recipes/timeout.rb
+++ cookbooks/timeout_test/recipes/timeout.rb
@@ -7,9 +7,11 @@
 # All rights reserved - Do Not Redistribute
 #
 
-rightscale_marker
+rightscale_marker :begin
 
 execute "this_should_hit_timeout" do
 	command "sleep 30s"
 	timeout 1
 end
+
+rightscale_marker :end
